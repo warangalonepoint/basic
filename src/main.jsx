a712be8144554d54ed9a74637f9d5fd85182aab3
Chaitanya <your@email.com>
2025-10-30 11:19:52 +0530

    Initial Onestop Ai commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DoctorPWA from "./DoctorPWA";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DoctorPWA />
+  </React.StrictMode>
+);
