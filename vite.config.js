2c609222a4535d50da81e696f8a03ab3c9d14a83
warangalonepoint <gullapallychaitanya@gmail.com>
2025-10-30 12:13:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/basic/',   // <-- repo name here
+  plugins: [react()], // IMPORTANT: no "base" for Vercel
 })
