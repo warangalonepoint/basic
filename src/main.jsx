79ae841ea499f260080fe33ecc6f4fca15db3cf7
warangalonepoint <gullapallychaitanya@gmail.com>
2025-10-30 13:20:24 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import DoctorPWA from './DoctorPWA.jsx'
-import './index.css'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<DoctorPWA />)
